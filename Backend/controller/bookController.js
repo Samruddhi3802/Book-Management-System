148653633583dca6bd90414aea8efdcdc6020e6a
Samruddhi3802 <sahindrakarsamruddhi01@gmail.com>
2026-03-05 10:28:48 +0530

    Backend crud done

--- Backend/controller/bookController.js
+++ Backend/controller/bookController.js
@@ -1,6 +1,6 @@
 const book=require("../model/book");
 
-const handleBookController=async (req, res)=>{
+const addBookController=async (req, res)=>{
  try{
    const data=req.body;
 
@@ -28,9 +28,111 @@ const handleBookController=async (req, res)=>{
  {
     return res.status(500).json({
         success:false,
-        message:"Internal server error"
+        message:err.message
     })
  }
 }
 
-module.exports={handleBookController}
+const getBookController=async (req, res)=>{
+    try{
+       const bookData=await book.find({});
+       if(bookData){
+        return res.status(200).json({
+            success:true,
+            message:"Books fetched successfully",
+            bookData,
+            totalBooks:bookData.length
+        })
+       }
+       return res.status(400).json({
+        success:false,
+        message:"Books not Found..."
+       })
+    }
+    catch(err){
+      return res.status(500).json({
+        success:false,
+        message:err.message
+      })
+    }
+}
+
+const getOneBookController=async(req, res)=>{
+  try{
+    let data=req.body;
+    let bookData=await book.findOne({_id:data.id});
+    if(bookData){
+        return res.status(200).json({
+            success:true,
+            message:"Book fetched successfully",
+            bookData
+        })
+    }
+    return res.status(400).json({
+        success:false,
+        message:"Book not Found..."
+    })
+  }
+  catch(err){
+    return res.status(500).json({
+        success:false,
+        message:err.message
+      })
+  }
+}
+
+const deleteOneBookController=async(req, res)=>{
+    try{
+      let data=req.body;
+      let response=await book.deleteOne({_id:data.id});
+      if(response.deletedCount>0){
+        return res.status(200).json({
+            success:true,
+            message:"Book deleted successfully"
+        })
+      }
+      return res.status(400).json({
+        success:false,
+        message:"Book not Found..."
+      })
+    }
+    catch(err){
+        return res.status(500).json({
+        success:false,
+        message:err.message
+      })
+    }
+}
+
+const updateOneBookController=async(req, res)=>{
+    try {
+        let data=req.body;
+        let response=await book.updateOne({_id:data.id},{
+            $set:{
+                bookTitle:data.bookTitle,
+                bookAuthor:data.bookAuthor,
+                publishDate:data.publishDate,
+                Price:data.Price,
+                Description:data.Description
+            }
+        })
+        if(response.modifiedCount>0){
+            res.status(200).json({
+                success:true,
+                message:"Book updated successfully",
+            })
+        }
+        return res.status(400).json({
+            success:false,
+            message:"Book not Found..."
+        })
+    } catch (error) {
+        return res.status(500).json({
+        success:false,
+        message:err.message
+      })
+    }
+
+}
+
+module.exports={addBookController, getBookController, getOneBookController, deleteOneBookController, updateOneBookController}
--- Backend/routes/bookRoute.js
+++ Backend/routes/bookRoute.js
@@ -1,12 +1,25 @@
 const express=require("express");
 const router=express.Router();
-const {handleBookController}=require("../controller/bookController");
+const {addBookController, getBookController, getOneBookController, 
+    deleteOneBookController, updateOneBookController}=require("../controller/bookController");
 
 router.get("/", (req, res)=>{
     res.send("Book Route is working successfully");
 })
 
 //adding book
-router.post("/add", handleBookController)
+router.post("/add", addBookController)
+
+//geting all books
+router.get("/getbooks", getBookController)
+
+//getting one book
+router.get("/getonebook", getOneBookController)
+
+//deleting one book
+router.delete("/deleteonebook", deleteOneBookController)
+
+//updating one book
+router.put("/updateonebook", updateOneBookController)
 
 module.exports=router;
